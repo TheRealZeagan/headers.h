63152d66d284d03f8fe28657fdc69eb70d3569ae
TheRealZeagan <jonaspepsimax@hotmail.com>
2015-11-16 10:33:39 +0100

    headers.h

---
+++ headers.h
@@ -0,0 +1,47 @@
+//Dette er navnet på biblioteket i alle skal adde
+#include "headers.h"
+
+//Program to compile - Or the raytracer itself.
+#include "main.c"
+//Leo
+#include "dataStructure.h"
+//Jonas
+#include "sphere.c"
+#include "sphere.h"
+
+//Ray Tracer addons
+#include "vectors.c"
+#include "vectors.h"
+//Leo Shading
+#include "shading.c"
+#include "shading.h"
+//
+#include "fogging.c"
+#include "fogging.h"
+//
+#include "reflection.c"
+#include "reflection.h"
+//
+#include "refraction.c"
+#include "refraction.h"
+//
+#include "transparency.c"
+#include "transparency.h"
+//
+#include "caustic.c"
+#include "caustic.h"
+//
+#include "indirect_illumination.c"
+#include "indirect_illumination.h"
+//
+#include "textureMapping.c"
+#include "textureMapping.h"
+
+//#include "bumpMapping.h"
+//#include "bumpMapping.h"
+//#include "depthOfField.c"
+//#include "depthOfField.h"
+//#include "motionBlur.c"
+//#include "motionBlur.h"
+//#include "diffraction.c"
+//#include "diffraction.h"
